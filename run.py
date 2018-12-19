1c1e5020ba629a70ca248f7b6007dcfed1cc4d0d
Tauras Semaska <tstauras100@gmail.com>
2018-12-19 08:41:16 +0000

    initial commit

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello there!</h1>"
+    return "to send a message use /USERNAME/MESSAGE "
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
     
